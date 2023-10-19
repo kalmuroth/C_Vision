ec9d7d7d5b4ee86d96f59aadcbbf1ddec634bfaf
kalmuroth <dokkan.dracktar@gmail.com>
2023-10-19 16:14:54 +0200

    basic scan

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
 
 User? loggedinUser;
 
@@ -10,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final _auth = FirebaseAuth.instance;
+  String result = '';
 
   void initState() {
     super.initState();
@@ -31,25 +33,27 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: null,
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.close),
-              onPressed: () {
-                _auth.signOut();
-                Navigator.pop(context);
-
-                //Implement logout functionality
-              }),
-        ],
-        title: Text('Home Page'),
-        backgroundColor: Colors.lightBlueAccent,
-      ),
       body: Center(
-        child: Text(
-          "Welcome User",
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                var res = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SimpleBarcodeScannerPage(),
+                    ));
+                setState(() {
+                  if (res is String) {
+                    result = res;
+                  }
+                });
+              },
+              child: const Text('Open Scanner'),
+            ),
+            Text('Barcode Result: $result'),
+          ],
         ),
       ),
     );
