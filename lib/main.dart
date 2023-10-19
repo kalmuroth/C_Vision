2ad321238a430a580738f574d77681d04a285cbf
kalmuroth <dokkan.dracktar@gmail.com>
2023-10-19 15:34:50 +0200

    firebase config + login/register auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
-
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'welcome_screen.dart';
+import 'home_screen.dart';
+import 'signup_screen.dart';
+import 'login_screen.dart';
 
 void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+    apiKey: "AIzaSyDtjVzfF2BAoMlNKeXNhUNUzWx2b-hmvvA",
+    authDomain: "cvision-943d3.firebaseapp.com",
+    projectId: "cvision-943d3",
+    storageBucket: "cvision-943d3.appspot.com",
+    messagingSenderId: "999491928239",
+    appId: "1:999491928239:web:55a00db8b5d5a7663b145a",
+    measurementId: "G-5SXP105SK9"
+    ),
+  );
+  runApp(MyApp());
+}
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: 'welcome_screen',
+      routes: {
+        'welcome_screen': (context) => WelcomeScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'home_screen': (context) => HomeScreen()
+      },
+    );
+  }
 }
